1736fc9abfb4b82c28159527e2ecc72bcb522cf1
Larry Landon <larryl011010@gmail.com>
2016-12-28 15:04:48 -0600

    Refactor logging, add a bit of pattern matching/validation

--- SafetyPy/SafetyPy.py
+++ SafetyPy/SafetyPy.py
@@ -42,10 +42,9 @@ class safetyculture:
 
     def parse_api_token(self, config):
         logger = logging.getLogger('sp_logger')
-
         try:
             api_token = config['API']['token']
-            token_is_valid = re.match('[a-z0-9]{64}', api_token)
+            token_is_valid = re.match('^[a-z0-9]{64}$', api_token)
             if token_is_valid:
                 logger.debug('API token matched pattern')
                 return api_token
@@ -56,11 +55,19 @@ class safetyculture:
             self.log_exception(ex, 'Exception parsing API token from config.yaml')
             return None
 
-
     def log_exception(self, ex, message):
         logger = logging.getLogger('sp_logger')
         logger.critical(message)
         logger.critical(ex)
+
+    def log_http_status(self, status_code, message):
+        logger = logging.getLogger('sp_logger')
+        status_description = requests.status_codes._codes[status_code][0]
+        log_string = str(status_code) + ' [' + status_description + '] status received ' + message
+        if status_code == requests.codes.ok:
+            logger.info(log_string)
+        else:
+            logger.error(log_string)
 
     def configure_logging(self):
         log_level = logging.DEBUG
@@ -80,7 +87,6 @@ class safetyculture:
         sh.setFormatter(formatter)
         sp_logger.addHandler(sh)
 
-
     def validate_log_directory(self, log_dir):
         """
         check for log subdirectory (current directory + '/log/')
@@ -98,13 +104,12 @@ class safetyculture:
                                                as completed, default to False
         Passing no parameters, it will return all audits with no restrictions
         """
-
         logger = logging.getLogger('sp_logger')
 
-        if modified_after is None:
-            last_modified = '2000-01-01T00:00:00.000Z'
-        else:
+        if modified_after is not None:
             last_modified = modified_after
+        else:
+            last_modified = '2000-01-01T00:00:00.000Z'
 
         search_url = self.audit_url + 'search?field=audit_id&field=modified_at&modified_after=' + last_modified
         log_string = '\nInitiating audit_discovery with the parameters: ' + '\n'
@@ -113,23 +118,21 @@ class safetyculture:
         log_string += 'completed      = ' + str(completed) + '\n'
         logger.info(log_string)
 
-        if template_id:
+        if template_id is not None:
             search_url += '&template=' + template_id
-
-        if completed:
+        if completed is not False:
             search_url += '&completed=true'
 
-        results = requests.get(search_url, headers=self.auth_header)
-        status_code = results.status_code
-
-        if status_code == requests.codes.ok:
-            response = results.json()
-            logger.info(
-                str(status_code) + ' status received on audit_discovery: ' + str(response['total']) + ' discovered')
-            return response
+        response = requests.get(search_url, headers=self.auth_header)
+        if response.status_code == requests.codes.ok:
+            result = response.json()
+            log_message = 'on audit_discovery: ' + str(result['total']) + ' discovered'
         else:
-            logger.error(str(status_code) + ' status received on audit_discovery using ' + search_url)
-            return None
+            log_message = 'on audit_discovery using ' + search_url
+            result = None
+
+        self.log_http_status(response.status_code, log_message)
+        return result
 
     def discover_templates(self, modified_after=None, modified_before=None):
         """
@@ -140,33 +143,47 @@ class safetyculture:
 
         Passing no parameters, it will discover templates with no restrictions
         """
-        logger = logging.getLogger('sp_logger')
-
         search_url = self.template_search_url
         if modified_before is not None:
             search_url += '&modified_before=' + modified_before
         if modified_after is not None:
             search_url += '&modified_after=' + modified_after
 
-        template_ids = requests.get(search_url, headers=self.auth_header)
+        response = requests.get(search_url, headers=self.auth_header)
+        if response.status_code == requests.codes.ok:
+            result = response.json()
+            log_message = ' received on template discovery'
+        else:
+            result = None
+            log_message = ' received on template discovery using ' + search_url
 
-        logger.info(str(template_ids.status_code) + ' received on template discovery')
-
-        return template_ids.json()
+        self.log_http_status(response.status_code, log_message)
+        return result
 
     def get_export_profile(self, export_profile_id):
         """
         :param export_profile_id:  Export profile id of the profile to retrieve
         :return:                   Export profile in JSON format
         """
-        logger = logging.getLogger('sp_logger')
-        export_profile_url = self.api_url + "/export_profiles/" + export_profile_id
-        response = requests.get(export_profile_url, headers=self.auth_header)
-        logger.info(str(response.status_code) + ' status received on export profile retrieval')
-        if response.status_code == requests.codes.ok:
-            export_profile = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(response.content)
-            return export_profile
+        profile_id_pattern = '^template_[a-fA-F0-9]{32}:[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}$'
+        profile_id_is_valid = re.match(profile_id_pattern, export_profile_id)
 
+        if profile_id_is_valid:
+            export_profile_url = self.api_url + "/export_profiles/" + export_profile_id
+
+            response = requests.get(export_profile_url, headers=self.auth_header)
+            if response.status_code == requests.codes.ok:
+                result = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(response.content)
+                log_message = ' status received on export profile retrieval of ' + export_profile_id
+            else:
+                log_message = ' received while attempting to retrieve ' + export_profile_id
+                result = None
+
+            self.log_http_status(response.status_code, log_message)
+            return result
+        else:
+            self.log_exception(ValueError, 'export_profile_id %s does not match pattern' % export_profile_id)
+            return None
 
     def get_export_job_id(self, audit_id, timezone=DEFAULT_EXPORT_TIMEZONE, export_profile_id=None):
         """
@@ -177,9 +194,22 @@ class safetyculture:
         """
         export_url = self.audit_url + audit_id + '/export?format=pdf&timezone=' + timezone
         if export_profile_id is not None:
-            export_url += '&export_profile=' + export_profile_id
-        export_response = requests.post(export_url, headers=self.auth_header)
-        return export_response.json()
+            profile_id_pattern = '^template_[a-fA-F0-9]{32}:[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}$'
+            profile_id_is_valid = re.match(profile_id_pattern, export_profile_id)
+            if profile_id_is_valid:
+                export_url += '&export_profile=' + export_profile_id
+            else:
+                self.log_exception(ValueError, 'export_profile_id %s does not match pattern' % export_profile_id)
+
+        response = requests.post(export_url, headers=self.auth_header)
+        log_message = ' received on request to ' + export_url
+        if response.status_code == requests.codes.ok:
+            result = response.json()
+        else:
+            result = None
+
+        self.log_http_status(response.status_code, log_message)
+        return result
 
     def poll_for_export(self, audit_id, export_job_id):
         """
@@ -187,45 +217,49 @@ class safetyculture:
                       export_job_id of the export to poll for
         Return:      href for export download
         """
-        delay = .5
-        poll_url = self.audit_url + audit_id + '/exports/' + export_job_id
-        export_attempts = 1
-        poll_status = requests.get(poll_url, headers=self.auth_header)
-        status = poll_status.json()
-        logger = logging.getLogger('sp_logger')
-        if 'status' in status.keys():
-            if status['status'] == 'IN PROGRESS':
-                logger.info(str(status['status']) + ' : ' + audit_id)
-                time.sleep(delay)
-                return self.poll_for_export(audit_id, export_job_id)
+        job_id_pattern = '^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
+        job_id_is_valid = re.match(job_id_pattern, export_job_id)
+        if job_id_is_valid:
+            delay = .5
+            poll_url = self.audit_url + audit_id + '/exports/' + export_job_id
+            export_attempts = 1
+            poll_status = requests.get(poll_url, headers=self.auth_header)
+            status = poll_status.json()
+            logger = logging.getLogger('sp_logger')
+            if 'status' in status.keys():
+                if status['status'] == 'IN PROGRESS':
+                    logger.info(str(status['status']) + ' : ' + audit_id)
+                    time.sleep(delay)
+                    return self.poll_for_export(audit_id, export_job_id)
 
-            elif status['status'] == 'SUCCESS':
-                logger.info(str(status['status']) + ' : ' + audit_id)
-                return status['href']
-
-        else:
-            if export_attempts < 2:
-                export_attempts += 1
-                logger.info('retrying export process for: ' + audit_id)
-                retry_id = self.get_export_job_id(audit_id)
-                return self.poll_for_export(audit_id, retry_id['id'])
+                elif status['status'] == 'SUCCESS':
+                    logger.info(str(status['status']) + ' : ' + audit_id)
+                    return status['href']
             else:
-                logger.error("export for " + audit_id + " failed more than once - skipping it and moving on")
+                if export_attempts < 2:
+                    export_attempts += 1
+                    logger.info('retrying export process for: ' + audit_id)
+                    retry_id = self.get_export_job_id(audit_id)
+                    return self.poll_for_export(audit_id, retry_id['id'])
+                else:
+                    logger.error("export for " + audit_id + " failed more than once - skipping")
+        else:
+            self.log_exception(ValueError, 'export_job_id %s does not match expected pattern' % export_job_id)
 
     def download_pdf(self, pdf_href):
         """
         Parameters:  pdf_href:  href obtained from poll_for_export for export doc to download
         Returns:     String representation of pdf document
         """
+        response = requests.get(pdf_href, headers=self.auth_header)
+        log_message = ' status received on GET for href: ' + pdf_href
+        if response.status_code == requests.codes.ok:
+            results = response.content
+        else:
+            results = None
 
-        logger = logging.getLogger('sp_logger')
-
-        doc_file = requests.get(pdf_href, headers=self.auth_header)
-
-        logger.info(str(doc_file.status_code) + ' status received on GET for href: ' + pdf_href)
-
-        return doc_file.content
-
+        self.log_http_status(response.status_code, log_message)
+        return results
 
     def get_pdf(self, audit_id, timezone=DEFAULT_EXPORT_TIMEZONE, export_profile_id=None):
         """
@@ -244,14 +278,13 @@ class safetyculture:
         Parameters: audit_id of document to fetch
         Returns:    JSON audit object
         """
+        response = requests.get(self.audit_url + audit_id, headers=self.auth_header)
+        log_message = ' status received on GET for ' + audit_id
 
-        logger = logging.getLogger('sp_logger')
-
-        get_doc = requests.get(self.audit_url + audit_id, headers=self.auth_header)
-
-        if get_doc.status_code == requests.codes.ok:
-            logger.info(str(get_doc.status_code) + ' status received on GET for ' + audit_id)
-            doc_json = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(get_doc.content)
-            return doc_json
+        if response.status_code == requests.codes.ok:
+            result = json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(response.content)
         else:
-            logger.error(str(get_doc.status_code) + ' status received on GET for ' + audit_id)
+            result = None
+
+        self.log_http_status(response.status_code, log_message)
+        return result
