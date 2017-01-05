3d796bc978720ff58b2c409d438cc7ee27e73827
Larry Landon <larryl011010@gmail.com>
2017-01-05 16:01:42 -0600

    slight modification to unit tests

--- SafetyPy/test_SafetyPy.py
+++ SafetyPy/test_SafetyPy.py
@@ -12,13 +12,13 @@ class SafetyPyTestCase(unittest.TestCase):
     def test_parse_malformed_api_token(self):
         for bad_token in ['123', '111z09cg1e29c43hb77ea0di52ae37988c13f4b7fa6f6f59e9a1649141ef5132', '', None, 123]:
             config = {'API' : {'token' : bad_token}}
-            self.assertIsNone(sc_client.parse_api_token(config))
-            self.assertIsNone(sc_client.parse_api_token(config))
+            message = '{0} should cause None to be returned'.format(str(bad_token))
+            self.assertIsNone(sc_client.parse_api_token(config), msg=message)
 
     def test_parse_missing_api_token(self):
         for config in ['', None]:
-            self.assertIsNone(sc_client.parse_api_token(config))
-            self.assertIsNone(sc_client.parse_api_token(config))
+            message = '{0} should cause None to be returned'.format(str(config))
+            self.assertIsNone(sc_client.parse_api_token(config), msg=message)
 
 
 if __name__ == '__main__':
--- tools/exporter/test_exporter.py
+++ tools/exporter/test_exporter.py
@@ -10,15 +10,18 @@ class exporterTestCase(unittest.TestCase):
 
     def test_get_missing_export_profile_mapping(self):
         for config_settings in [{}, None, '']:
-            self.assertIsNone(ex.get_export_profile_mapping(config_settings))
+            message = '{0} should cause None to be returned'.format(str(config_settings))
+            self.assertIsNone(ex.get_export_profile_mapping(config_settings), msg=message)
 
     def test_get_missing_export_path(self):
         for config_settings in [{}, None, '']:
-            self.assertIsNone(ex.get_export_path(config_settings))
+            message = '{0} should cause None to be returned'.format(str(config_settings))
+            self.assertIsNone(ex.get_export_path(config_settings), msg=message)
 
     def test_get_missing_timezone(self):
         for config_settings in [{}, None, '']:
-            self.assertEquals(ex.get_timezone(config_settings), (str(get_localzone())))
+            message = '{0} should cause ' + str(get_localzone()) + ' to be returned'.format(str(config_settings))
+            self.assertEqual(ex.get_timezone(config_settings), (str(get_localzone())), msg=message)
 
 if __name__ == '__main__':
     unittest.main()
