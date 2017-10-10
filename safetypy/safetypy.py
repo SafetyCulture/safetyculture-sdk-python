ab2efe7a03a4fdf610025c339df2c5b9e340a77e
Larry Landon <larryl011010@gmail.com>
2017-10-10 14:17:27 -0500

    attempt to fix merge conflict...

--- safetypy/safetypy.py
+++ safetypy/safetypy.py
@@ -10,6 +10,7 @@ import re
 import sys
 import time
 import errno
+from builtins import input
 from datetime import datetime
 import requests
 from getpass import getpass
@@ -19,13 +20,14 @@ DEFAULT_EXPORT_FORMAT = 'pdf'
 GUID_PATTERN = '[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$'
 HTTP_USER_AGENT_ID = 'safetyculture-python-sdk'
 
+
 def get_user_api_token(logger):
     """
     Generate iAuditor API Token
     :param logger:  the logger
     :return:        API Token if authenticated else None
     """
-    username = raw_input("iAuditor username: ")
+    username = input("iAuditor username: ")
     password = getpass()
     generate_token_url = "https://api.safetyculture.io/auth"
     payload = "username=" + username + "&password=" + password + "&grant_type=password"
@@ -49,22 +51,20 @@ class SafetyCulture:
         self.api_url = 'https://api.safetyculture.io/'
         self.audit_url = self.api_url + 'audits/'
         self.template_search_url = self.api_url + 'templates/search?field=template_id&field=name'
-        self.response_set_url = self.api_url + 'response_sets'
 
         self.create_directory_if_not_exists(self.log_dir)
         self.configure_logging()
         logger = logging.getLogger('sp_logger')
         try:
             token_is_valid = re.match('^[a-f0-9]{64}$', api_token)
+            if token_is_valid:
+                self.api_token = api_token
+            else:
+                logger.error('API token failed to match expected pattern')
+                self.api_token = None
         except Exception as ex:
             self.log_critical_error(ex, 'Error occurred while validating API token in config.yaml file. Exiting.')
             exit()
-        if token_is_valid:
-            self.api_token = api_token
-        else:
-            logger.error('API token failed to match expected pattern')
-            self.api_token = None
-
         if self.api_token:
             self.custom_http_headers = {
                 'User-Agent': HTTP_USER_AGENT_ID,
@@ -78,24 +78,24 @@ class SafetyCulture:
         return requests.get(url, headers=self.custom_http_headers)
 
     def authenticated_request_post(self, url, data):
-        return requests.post(url, data, headers=self.custom_http_headers)
+        self.custom_http_headers['content-type'] = 'application/json'
+        response = requests.post(url, data, headers=self.custom_http_headers)
+        del self.custom_http_headers['content-type']
+        return response
 
-    def authenticated_request_delete(self, url):
-        return requests.delete(url, headers=self.custom_http_headers)
-
-    def parse_json(self, json_to_parse):
+    @staticmethod
+    def parse_json(json_to_parse):
         """
         Parse JSON string to OrderedDict and return
-
         :param json_to_parse:  string representation of JSON
         :return:               OrderedDict representation of JSON
         """
-        return json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(json_to_parse)
+        return json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(json_to_parse.decode('utf-8'))
 
-    def log_critical_error(self, ex, message):
+    @staticmethod
+    def log_critical_error(ex, message):
         """
         Write exception and description message to log
-
         :param ex:       Exception instance to log
         :param message:  Descriptive message to describe exception
         """
@@ -129,9 +129,7 @@ class SafetyCulture:
     def create_directory_if_not_exists(self, path):
         """
         Creates 'path' if it does not exist
-
         If creation fails, an exception will be thrown
-
         :param path:    the path to ensure it exists
         """
         try:
@@ -181,7 +179,6 @@ class SafetyCulture:
     def discover_templates(self, modified_after=None, modified_before=None):
         """
         Query API for all template IDs if no parameters are passed, otherwise restrict search based on parameters
-
         :param modified_after:   Restrict discovery to templates modified after this UTC timestamp
         :param modified_before:  Restrict discovery to templates modified before this UTC timestamp
         :return:                 JSON object containing IDs of all templates returned by API
@@ -215,7 +212,6 @@ class SafetyCulture:
     def get_export_profile(self, export_profile_id):
         """
         Query API for export profile corresponding to passed profile_id
-
         :param export_profile_id:  Export profile ID of the profile to retrieve
         :return:                   Export profile in JSON format
         """
@@ -239,7 +235,6 @@ class SafetyCulture:
                           export_format=DEFAULT_EXPORT_FORMAT):
         """
         Request export job ID from API and return it
-
         :param audit_id:           audit_id to retrieve export_job_id for
         :param timezone:           timezone to apply to exports
         :param export_profile_id:  export profile to apply to exports
@@ -309,7 +304,6 @@ class SafetyCulture:
 
     def download_export(self, export_href):
         """
-
         :param export_href:  href for export document to download
         :return:             String representation of exported document
         """
@@ -329,7 +323,6 @@ class SafetyCulture:
                    export_format=DEFAULT_EXPORT_FORMAT):
         """
         Obtain exported document from API and return string representation of it
-
         :param audit_id:           audit_id of export to obtain
         :param timezone:           timezone to apply to exports
         :param export_profile_id:  ID of export profile to apply to exports
@@ -345,9 +338,9 @@ class SafetyCulture:
     def get_media(self, audit_id, media_id):
         """
         Get media item associated with a specified audit and media ID
-        :param audit_id:    audit ID of document that contains media 
+        :param audit_id:    audit ID of document that contains media
         :param media_id:    media ID of image to fetch
-        :return:            The Content-Type will be the MIME type associated with the media, 
+        :return:            The Content-Type will be the MIME type associated with the media,
                             and the body of the response is the media itself.
         """
         url = self.audit_url + audit_id + '/media/' + media_id
@@ -369,10 +362,49 @@ class SafetyCulture:
         else:
             return None
 
+    def get_audit_actions(self, date_modified, offset=0, page_length=100):
+        """
+        Get all actions created after a specified date. If the number of actions found is more than 100, this function will
+        page until it has collected all actions
+        :param date_modified:   ISO formatted date/time string. Only actions created after this date are are returned.
+        :param offset:          The index to start retrieving actions from
+        :param page_length:     How many actions to fetch for each page of action results
+        :return:                Array of action objects
+        """
+        logger = logging.getLogger('sp_logger')
+        actions_url = self.api_url + 'actions/search'
+        response = self.authenticated_request_post(
+            actions_url,
+            data=json.dumps({"modified_at": {"from": str(date_modified)}, "offset": offset})
+        )
+        result = self.parse_json(response.content) if response.status_code == requests.codes.ok else None
+        self.log_http_status(response.status_code, 'GET actions')
+        if result is None or None in [result.get('count'), result.get('offset'), result.get('total'), result.get('actions')]:
+            return None
+        return self.get_page_of_actions(logger, date_modified, result, offset, page_length)
+
+    def get_page_of_actions(self, logger, date_modified, previous_page, offset=0, page_length=100):
+        """
+        Returns a page of action search results
+        :param logger: the logger
+        :param date_modified: fetch from that date onwards
+        :param previous_page: a page of action search results
+        :param offset: the index to start retrieving actions from
+        :param page_length: the number of actions to return on the search page (max 100)
+        :return: Array of action objects
+        """
+        if previous_page['count'] + previous_page['offset'] < previous_page['total']:
+            logger.info('Paging Actions. Offset: ' + str(offset + page_length) + '. Total: ' + str(previous_page['total']))
+            next_page = self.get_audit_actions(date_modified, offset + page_length)
+            if next_page is None:
+                return None
+            return next_page + previous_page['actions']
+        elif previous_page['count'] + previous_page['offset'] == previous_page['total']:
+            return previous_page['actions']
+
     def get_audit(self, audit_id):
         """
         Request JSON representation of a single specified audit and return it
-
         :param audit_id:  audit_id of document to fetch
         :return:          JSON audit object
         """
@@ -383,73 +415,13 @@ class SafetyCulture:
         self.log_http_status(response.status_code, log_message)
         return result
 
-    def create_response_set(self, name, responses):
-        """
-        Create new response_set
-        :param payload:  Name and responses of response_set to create
-        :return:
-        """
-        payload = json.dumps({'name': name, 'responses': responses})
-        response = self.authenticated_request_post(self.response_set_url, payload)
-        # result = self.parse_json(response.content) if response.status_code == requests.codes.ok else None
-        log_message = 'on POST for new response_set: {0}'.format(name)
-        self.log_http_status(response.status_code, log_message)
-
-    def get_response_sets(self):
-        """
-        GET and return all response_sets
-        :return: response_sets accessible to user
-        """
-        response = self.authenticated_request_get(self.response_set_url)
-        result = self.parse_json(response.content) if response.status_code == requests.codes.ok else None
-        log_message = 'on GET for response_sets'
-        self.log_http_status(response.status_code, log_message)
-        return result
-
-    def get_response_set(self, responseset_id):
-        """
-        GET individual response_set by id
-        :param responseset_id:  responseset_id of response_set to GET
-        :return: response_set
-        """
-        response = self.authenticated_request_get('{0}/{1}'.format(self.response_set_url, responseset_id))
-        result = self.parse_json(response.content) if response.status_code == requests.codes.ok else None
-        log_message = 'on GET for {0}'.format(responseset_id)
-        self.log_http_status(response.status_code, log_message)
-        return result
-
-    def create_response(self, responseset_id, response):
-        """
-        Create response in existing response_set
-        :param responseset_id: id of response_set to add response to
-        :param response:       response to add
-        :return:               None
-        """
-        url = '{0}/{1}/responses'.format(self.response_set_url, responseset_id)
-        response = self.authenticated_request_post(url, json.dumps(response))
-        log_message = 'on POST for new response to: {0}'.format(responseset_id)
-        self.log_http_status(response.status_code, log_message)
-
-    def delete_response(self, responseset_id, response_id):
-        """
-        DELETE individual response by id
-        :param responseset_id: responseset_id of response_set containing response to be deleted
-        :param response_id:    id of response to be deleted
-        :return:               None
-        """
-        url = '{0}/{1}/responses/{2}'.format(self.response_set_url, responseset_id, response_id)
-        response = self.authenticated_request_delete(url)
-        log_message = 'on DELETE for response_set: {0}'.format(responseset_id)
-        self.log_http_status(response.status_code, log_message)
-
-    def log_http_status(self, status_code, message):
+    @staticmethod
+    def log_http_status(status_code, message):
         """
         Write http status code and descriptive message to log
-
         :param status_code:  http status code to log
         :param message:      to describe where the status code was obtained
         """
-
         logger = logging.getLogger('sp_logger')
         status_description = requests.status_codes._codes[status_code][0]
         log_string = str(status_code) + ' [' + status_description + '] status received ' + message
