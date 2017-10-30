371eeaed721c13f415568ce239ce01515e6fa05d
Tony Oreglia <tony.oreglia@safetyculture.com>
2017-10-30 17:04:48 +1100

    added fxn to parse API region from config.yaml. Added parameter to safetypy to accept an API region in constructor

--- safetypy/safetypy.py
+++ safetypy/safetypy.py
@@ -44,10 +44,12 @@ def get_user_api_token(logger):
 
 
 class SafetyCulture:
-    def __init__(self, api_token):
+    def __init__(self, api_token, api_region):
         self.current_dir = os.getcwd()
         self.log_dir = self.current_dir + '/log/'
 
+        # logic will go here to make the url dependent upon the API Region
+        # Region can be usa, aus, or uk
         self.api_url = 'https://api.safetyculture.io/'
         self.audit_url = self.api_url + 'audits/'
         self.template_search_url = self.api_url + 'templates/search?field=template_id&field=name'
