7a0744313c219917ad320535bf4dcb5d9fe2e44c
Edd Abrahamsen-Mills <edd@Edds-Work-MacBook-Pro.local>
2019-10-16 19:00:09 +0100

    Reversed changes

--- safetypy/safetypy.py
+++ safetypy/safetypy.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 # Author: SafetyCulture
 # Copyright: © SafetyCulture 2016
+# pylint: disable=E1101
 
 import collections
 import json
@@ -11,32 +12,19 @@ import sys
 import time
 import errno
 from builtins import input
+from datetime import datetime
 import requests
 from getpass import getpass
 
-DEFAULT_EXPORT_TIMEZONE = 'Etc/UTC'
-DEFAULT_EXPORT_FORMAT = 'pdf'
+DEFAULT_EXPORT_FORMAT = 'PDF'
 GUID_PATTERN = '[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$'
 HTTP_USER_AGENT_ID = 'safetyculture-python-sdk'
-
-# https://docs.python.org/2.7/howto/logging.html#configuring-logging-for-a-library
-try:
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def handle(self, record):
-            pass
-        def emit(self, record):
-            pass
-        def createLock(self):
-            self.lock = None
-logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
 def get_user_api_token(logger):
     """
     Generate iAuditor API Token
-    :param logger:  No longer used
+    :param logger:  the logger
     :return:        API Token if authenticated else None
     """
     username = input("iAuditor username: ")
@@ -51,11 +39,11 @@ def get_user_api_token(logger):
     if response.status_code == requests.codes.ok:
         return response.json()['access_token']
     else:
-        logging.error('An error occurred calling ' + generate_token_url + ': ' + str(response.json()))
+        logger.error('An error occurred calling ' + generate_token_url + ': ' + str(response.json()))
         return None
 
 
-class SafetyCulture(object):
+class SafetyCulture:
     def __init__(self, api_token):
         self.current_dir = os.getcwd()
         self.log_dir = self.current_dir + '/log/'
@@ -67,12 +55,15 @@ class SafetyCulture(object):
         self.all_groups_url = self.api_url + 'groups'
         self.add_users_url = self.api_url + 'users'
 
+        self.create_directory_if_not_exists(self.log_dir)
+        self.configure_logging()
+        logger = logging.getLogger('sp_logger')
         try:
             token_is_valid = re.match('^[a-f0-9]{64}$', api_token)
             if token_is_valid:
                 self.api_token = api_token
             else:
-                logging.error('API token failed to match expected pattern')
+                logger.error('API token failed to match expected pattern')
                 self.api_token = None
         except Exception as ex:
             self.log_critical_error(ex, 'API token is missing or invalid. Exiting.')
@@ -83,7 +74,7 @@ class SafetyCulture(object):
                 'Authorization': 'Bearer ' + self.api_token
             }
         else:
-            logging.error('No valid API token parsed! Exiting.')
+            logger.error('No valid API token parsed! Exiting.')
             sys.exit(1)
 
     def authenticated_request_get(self, url):
@@ -121,9 +112,32 @@ class SafetyCulture(object):
         :param ex:       Exception instance to log
         :param message:  Descriptive message to describe exception
         """
+        logger = logging.getLogger('sp_logger')
 
-        logging.critical(message)
-        logging.critical(ex)
+        if logger is not None:
+            logger.critical(message)
+            logger.critical(ex)
+
+    def configure_logging(self):
+        """
+        Configure logging to log to std output as well as to log file
+        """
+        log_level = logging.DEBUG
+
+        log_filename = datetime.now().strftime('%Y-%m-%d') + '.log'
+        sp_logger = logging.getLogger('sp_logger')
+        sp_logger.setLevel(log_level)
+        formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+
+        fh = logging.FileHandler(filename=self.log_dir + log_filename)
+        fh.setLevel(log_level)
+        fh.setFormatter(formatter)
+        sp_logger.addHandler(fh)
+
+        sh = logging.StreamHandler(sys.stdout)
+        sh.setLevel(log_level)
+        sh.setFormatter(formatter)
+        sp_logger.addHandler(sh)
 
     def create_directory_if_not_exists(self, path):
         """
@@ -152,6 +166,8 @@ class SafetyCulture(object):
         :return:                JSON object containing IDs of all audits returned by API
         """
 
+        logger = logging.getLogger('sp_logger')
+
         last_modified = modified_after if modified_after is not None else '2000-01-01T00:00:00.000Z'
 
         search_url = self.audit_url + 'search?field=audit_id&field=modified_at&order=asc&modified_after=' \
@@ -160,7 +176,7 @@ class SafetyCulture(object):
         log_string += 'template_id    = ' + str(template_id) + '\n'
         log_string += 'modified_after = ' + str(last_modified) + '\n'
         log_string += 'completed      = ' + str(completed) + '\n'
-        logging.info(log_string)
+        logger.info(log_string)
 
         if template_id is not None:
             for specific_id in template_id:
@@ -201,66 +217,44 @@ class SafetyCulture(object):
         self.log_http_status(response.status_code, log_message)
         return result
 
-    def get_export_profile_ids(self, template_id=None):
+    def get_preference_ids(self, template_id=None):
         """
-        Query API for all export profile IDs if no parameters are passed, else restrict to template_id passed
-        :param template_id: template_id to obtain export profiles for
-        :return:            JSON object containing template name: export profile pairs if no errors, or None
+        Query API for all preference IDs if no parameters are passed, else restrict to template_id passed
+        :param template_id: template_id to obtain export preferences for
+        :return:            JSON object containing list of preference objects
         """
-        profile_search_url = self.api_url + 'export_profiles/search'
+        preference_search_url = self.api_url + 'preferences/search'
         if template_id is not None:
-            profile_search_url += '?template=' + template_id
-        response = self.authenticated_request_get(profile_search_url)
+            preference_search_url += '?template_id=' + template_id
+        response = self.authenticated_request_get(preference_search_url)
         result = response.json() if response.status_code == requests.codes.ok else None
         return result
 
-    def get_export_profile(self, export_profile_id):
-        """
-        Query API for export profile corresponding to passed profile_id
-
-        :param export_profile_id:  Export profile ID of the profile to retrieve
-        :return:                   Export profile in JSON format
-        """
-        profile_id_pattern = '^template_[a-fA-F0-9]{32}:' + GUID_PATTERN
-        profile_id_is_valid = re.match(profile_id_pattern, export_profile_id)
-
-        if profile_id_is_valid:
-            export_profile_url = self.api_url + '/export_profiles/' + export_profile_id
-            response = self.authenticated_request_get(export_profile_url)
-            result = self.parse_json(response.content) if response.status_code == requests.codes.ok else None
-            log_message = 'on export profile retrieval of ' + export_profile_id
-
-            self.log_http_status(response.status_code, log_message)
-            return result
-        else:
-            self.log_critical_error(ValueError,
-                                    'export_profile_id {0} does not match expected pattern'.format(export_profile_id))
-            return None
-
-    def get_export_job_id(self, audit_id, timezone=DEFAULT_EXPORT_TIMEZONE, export_profile_id=None,
-                          export_format=DEFAULT_EXPORT_FORMAT):
+    def get_export_job_id(self, audit_id, preference_id=None, export_format=DEFAULT_EXPORT_FORMAT):
         """
         Request export job ID from API and return it
 
         :param audit_id:           audit_id to retrieve export_job_id for
-        :param timezone:           timezone to apply to exports
-        :param export_profile_id:  export profile to apply to exports
+        :param preference_id:      preference to apply to exports
         :param export_format:      desired format of exported document
         :return:                   export job ID obtained from API
         """
-        export_url = self.audit_url + audit_id + '/export?format=' + export_format + '&timezone=' + timezone
+        export_url = self.audit_url + audit_id + '/report'
+        if export_format == 'docx': # convert old command line format
+            export_format = 'WORD'
+        export_data = {'format': export_format.upper()}
 
-        if export_profile_id is not None:
-            profile_id_pattern = '^template_[a-fA-F0-9]{32}:' + GUID_PATTERN
-            profile_id_is_valid = re.match(profile_id_pattern, export_profile_id)
-            if profile_id_is_valid:
-                export_url += '&export_profile=' + export_profile_id
+        if preference_id is not None:
+            preference_id_pattern = '^template_[a-fA-F0-9]{32}:' + GUID_PATTERN
+            preference_id_is_valid = re.match(preference_id_pattern, preference_id)
+            if preference_id_is_valid:
+                export_data['preference_id'] = preference_id.split(':')[1]
             else:
                 self.log_critical_error(ValueError,
-                                        'export_profile_id {0} does not match expected pattern'.format(
-                                            export_profile_id))
+                                        'preference_id {0} does not match expected pattern'.format(
+                                            preference_id))
 
-        response = self.authenticated_request_post(export_url, data=None)
+        response = self.authenticated_request_post(export_url, data=json.dumps(export_data))
         result = response.json() if response.status_code == requests.codes.ok else None
         log_message = 'on request to ' + export_url
 
@@ -279,30 +273,31 @@ class SafetyCulture(object):
 
         if job_id_is_valid:
             delay_in_seconds = 5
-            poll_url = self.audit_url + audit_id + '/exports/' + export_job_id
+            poll_url = self.audit_url + audit_id + '/report/' + export_job_id
             export_attempts = 1
             poll_status = self.authenticated_request_get(poll_url)
             status = poll_status.json()
+            logger = logging.getLogger('sp_logger')
             if 'status' in status.keys():
-                if status['status'] == 'IN PROGRESS':
-                    logging.info(str(status['status']) + ' : ' + audit_id)
+                if status['status'] == 'IN_PROGRESS':
+                    logger.info(str(status['status']) + ' : ' + audit_id)
                     time.sleep(delay_in_seconds)
                     return self.poll_for_export(audit_id, export_job_id)
 
                 elif status['status'] == 'SUCCESS':
-                    logging.info(str(status['status']) + ' : ' + audit_id)
-                    return status['href']
+                    logger.info(str(status['status']) + ' : ' + audit_id)
+                    return status['url']
 
                 else:
                     if export_attempts < 2:
                         export_attempts += 1
-                        logging.info('attempt # {0} exporting report for: ' + audit_id.format(str(export_attempts)))
+                        logger.info('attempt # {0} exporting report for: ' + audit_id.format(str(export_attempts)))
                         retry_id = self.get_export_job_id(audit_id)
-                        return self.poll_for_export(audit_id, retry_id['id'])
+                        return self.poll_for_export(audit_id, retry_id['messageId'])
                     else:
-                        logging.error('export for ' + audit_id + ' failed {0} times - skipping'.format(export_attempts))
+                        logger.error('export for ' + audit_id + ' failed {0} times - skipping'.format(export_attempts))
             else:
-                logging.critical('Unexpected response from API: {0}'.format(status))
+                logger.critical('Unexpected response from API: {0}'.format(status))
 
         else:
             self.log_critical_error(ValueError,
@@ -326,18 +321,16 @@ class SafetyCulture(object):
         except Exception as ex:
             self.log_critical_error(ex, 'Exception occurred while attempting download_export({0})'.format(export_href))
 
-    def get_export(self, audit_id, timezone=DEFAULT_EXPORT_TIMEZONE, export_profile_id=None,
-                   export_format=DEFAULT_EXPORT_FORMAT):
+    def get_export(self, audit_id, preference_id=None, export_format=DEFAULT_EXPORT_FORMAT):
         """
         Obtain exported document from API and return string representation of it
 
         :param audit_id:           audit_id of export to obtain
-        :param timezone:           timezone to apply to exports
-        :param export_profile_id:  ID of export profile to apply to exports
+        :param preference_id:  ID of preference to apply to exports
         :param export_format:      desired format of exported document
         :return:                   String representation of exported document
         """
-        export_job_id = self.get_export_job_id(audit_id, timezone, export_profile_id, export_format)['id']
+        export_job_id = self.get_export_job_id(audit_id, preference_id, export_format)['messageId']
         export_href = self.poll_for_export(audit_id, export_job_id)
 
         export_content = self.download_export(export_href)
@@ -380,6 +373,7 @@ class SafetyCulture(object):
         :param page_length:     How many actions to fetch for each page of action results
         :return:                Array of action objects
         """
+        logger = logging.getLogger('sp_logger')
         actions_url = self.api_url + 'actions/search'
         response = self.authenticated_request_post(
             actions_url,
@@ -393,12 +387,13 @@ class SafetyCulture(object):
         self.log_http_status(response.status_code, 'GET actions')
         if result is None or None in [result.get('count'), result.get('offset'), result.get('total'), result.get('actions')]:
             return None
-        return self.get_page_of_actions(date_modified, result, offset, page_length)
+        return self.get_page_of_actions(logger, date_modified, result, offset, page_length)
 
-    def get_page_of_actions(self, date_modified, previous_page, offset=0, page_length=100):
+    def get_page_of_actions(self, logger, date_modified, previous_page, offset=0, page_length=100):
         """
         Returns a page of action search results
 
+        :param logger: the logger
         :param date_modified: fetch from that date onwards
         :param previous_page: a page of action search results
         :param offset: the index to start retrieving actions from
@@ -406,7 +401,7 @@ class SafetyCulture(object):
         :return: Array of action objects
         """
         if previous_page['count'] + previous_page['offset'] < previous_page['total']:
-            logging.info('Paging Actions. Offset: ' + str(offset + page_length) + '. Total: ' + str(previous_page['total']))
+            logger.info('Paging Actions. Offset: ' + str(offset + page_length) + '. Total: ' + str(previous_page['total']))
             next_page = self.get_audit_actions(date_modified, offset + page_length)
             if next_page is None:
                 return None
@@ -577,6 +572,7 @@ class SafetyCulture(object):
         :param status_code:  http status code to log
         :param message:      to describe where the status code was obtained
         """
+        logger = logging.getLogger('sp_logger')
         status_description = requests.status_codes._codes[status_code][0]
         log_string = str(status_code) + ' [' + status_description + '] status received ' + message
-        logging.info(log_string) if status_code == requests.codes.ok else logging.error(log_string)
+        logger.info(log_string) if status_code == requests.codes.ok else logger.error(log_string)
